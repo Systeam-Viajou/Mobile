6ee198df1c0767b0e24b1caafad9119f57ebf097
Jampeta <joao.pedro@germinare.org.br>
2024-08-23 09:39:03 -0300

    Mudanças na tela login

--- app/src/main/java/com/interdiciplinar/viajou/Telas/TelasEntrada/TelaLogin.java
+++ app/src/main/java/com/interdiciplinar/viajou/Telas/TelasEntrada/TelaLogin.java
@@ -2,9 +2,12 @@ package com.interdiciplinar.viajou.Telas.TelasEntrada;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.interdiciplinar.viajou.R;
+import com.interdiciplinar.viajou.Telas.Animacoes.SplashScreen;
 
 public class TelaLogin extends AppCompatActivity {
 
@@ -12,5 +15,11 @@ public class TelaLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+    }
+
+    public void telaCadastro(View view) {
+        Intent intent = new Intent(TelaLogin.this, TelaCadastro.class);
+        startActivity(intent);
+        finish();
     }
 }
