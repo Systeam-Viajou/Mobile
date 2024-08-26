2a0aa817516e6f6e2be786ed2410564a984f3259
evellynNakamura <evellyn.nakamura@germinare.org.br>
2024-08-26 10:47:20 -0300

    card de eventos

---
+++ app/src/main/java/com/interdiciplinar/viajou/Telas/TelasPrincipais/TelaEventos.java
@@ -0,0 +1,16 @@
+package com.interdiciplinar.viajou.Telas.TelasPrincipais;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.interdiciplinar.viajou.R;
+
+public class TelaEventos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_eventos);
+    }
+}
