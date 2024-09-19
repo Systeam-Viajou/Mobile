692b961f64f827a5ad6acfe4203e46130fc75715
evellynNakamura <evellyn.nakamura@germinare.org.br>
2024-09-19 12:10:36 -0300

    xml da tela de home

---
+++ app/src/main/java/com/interdiciplinar/viajou/Telas/TelasPrincipais/model/CardHome.java
@@ -0,0 +1,6 @@
+package com.interdiciplinar.viajou.Telas.TelasPrincipais.model;
+
+public class CardHome {
+    private String nome;
+//    Esperar eles colocarem tanto o nome quanto a imagem no mongo
+}
