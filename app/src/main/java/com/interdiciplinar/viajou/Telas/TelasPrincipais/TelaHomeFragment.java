489575cd43f3bfc7a1775aece849cc7da989be2b
evellynNakamura <evellyn.nakamura@germinare.org.br>
2024-10-03 12:48:34 -0300

    adicionando adapters de cada recyclervuew da home

--- app/src/main/java/com/interdiciplinar/viajou/Telas/TelasPrincipais/TelaHomeFragment.java
+++ app/src/main/java/com/interdiciplinar/viajou/Telas/TelasPrincipais/TelaHomeFragment.java
@@ -23,9 +23,9 @@ import java.util.List;
 
 public class TelaHomeFragment extends Fragment {
 
-    RecyclerView recyclerContinuar, recyclerRecomendar;
-    private HomeRecomendarAdapter recomandarAdapter;
-    private List<Home> continuarList, recomendarlist;
+    RecyclerView recyclerContinuar, recyclerRecomendar, recyclerPopulares, recyclerPerto, recyclerExperiencia;
+    private HomeRecomendarAdapter recomendarAdapter, popularesAdapter, pertoAdapter, experienciaAdapter;
+    private List<Home> continuarList, recomendarlist, popularesList, pertoList, experienciaList;
 
     public TelaHomeFragment() {
         // Required empty public constructor
@@ -56,12 +56,24 @@ public class TelaHomeFragment extends Fragment {
         recyclerRecomendar = view.findViewById(R.id.recyclerRecomenHome);
         recyclerRecomendar.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
 
+
+        recyclerPopulares = view.findViewById(R.id.recyclerPopularesHome);
+        recyclerPopulares.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+
+        recyclerPerto = view.findViewById(R.id.recyclerPertoHome);
+        recyclerPerto.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+
+        recyclerExperiencia = view.findViewById(R.id.recyclerExperienciaHome);
+        recyclerExperiencia.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
         // Mockando os dados
         recomendarlist = getMockhomeRecomendar();
 
         // Inicializando o adapter com a lista de eventos mockados
-        recomandarAdapter = new HomeRecomendarAdapter(recomendarlist, getContext());
-        recyclerRecomendar.setAdapter(recomandarAdapter);
+        recomendarAdapter = new HomeRecomendarAdapter(recomendarlist, getContext());
+        recyclerRecomendar.setAdapter(recomendarAdapter);
 
 //        Button bt = view.findViewById(R.id.btTool);
 
@@ -75,6 +87,7 @@ public class TelaHomeFragment extends Fragment {
         recomendados.add(mockHome("Av. Paulista", "https://guiaviajarmelhor.com.br/wp-content/uploads/2022/01/avenida-paulista-passeios-baratos.jpg", "turismo"));
         recomendados.add(mockHome("Parque Ibirabuera", "https://www.melhoresdestinos.com.br/wp-content/uploads/2019/02/passagens-aereas-sao-paulo-capa2019-04-820x430.jpg", "turismo"));
         recomendados.add(mockHome("BGS", "https://s2-techtudo.glbimg.com/RXGM-wwhzTf7FVlyMIDnKdmTYdM=/0x0:768x476/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/7/Z/pex9eQQWu1dDxuBh3n3A/bgs-2023.jpg", "evento"));
+        recomendados.add(mockHome("Parque 2", "https://www.melhoresdestinos.com.br/wp-content/uploads/2019/02/passagens-aereas-sao-paulo-capa2019-04-820x430.jpg", "evento"));
 
         return recomendados;
     }
