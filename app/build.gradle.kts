90a7052af5f744ff38ecf01492231a2cea90c1c6
evellynNakamura <evellyn.nakamura@germinare.org.br>
2024-10-12 21:34:32 -0300

    arrumando cor de fundo da tela home e turismos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,9 +57,6 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    implementation ("com.google.code.gson:gson:2.8.9")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-
     implementation("com.github.bumptech.glide:glide:4.14.2")
 
 }
