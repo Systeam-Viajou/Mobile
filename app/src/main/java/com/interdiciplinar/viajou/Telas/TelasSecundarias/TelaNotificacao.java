56ee373ae7a74d52d0caadf5731bd17103c4e0c7
evellynNakamura <evellyn.nakamura@germinare.org.br>
2024-10-18 09:53:37 -0300

    tela de notificação e arrumando toolbar da home e de turismos

---
+++ app/src/main/java/com/interdiciplinar/viajou/Telas/TelasSecundarias/TelaNotificacao.java
@@ -0,0 +1,18 @@
+package com.interdiciplinar.viajou.Telas.TelasSecundarias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.interdiciplinar.viajou.R;
+
+public class TelaNotificacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_notificacao);
+
+
+    }
+}
